cffc1d4f5baf79e42870fe92de370b069f8c9090
Logan Frances <54649845+loganfrances@users.noreply.github.com>
2019-12-17 23:37:51 -0500

    Add files via upload

--- paths/bigbeanslilgabe/bean.js
+++ paths/bigbeanslilgabe/bean.js
@@ -1,17 +1,8 @@
 $(document).ready(function () {
-    $.(".calendar").click(function(){
-        var WOOF = $("#woof") [0];
+    var box = $(".calendar-box");
+    var WOOF = $("#woof")[0];
+    $(".calendar-box").click(function(){
         WOOF.play();
-        var text = "done!";
-        var box = $(".calendar")
-        var addTextToBox = function(){
-            var textBox = $("<span><span>");
-            textBox.addClass("text-box");
-            textBox.append(text);
-            box.append(textBox)
-        }
-        box.click(addTextToBox)
     });
-    box.click(WOOF.play());
 });
 
